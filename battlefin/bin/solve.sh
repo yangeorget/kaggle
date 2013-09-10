f6a64cfd25037f100c72b138050caba1e833d338
Yan Georget <y.georget@criteo.com>
2013-09-10 08:12:55 +0200

    broken

--- battlefin/bin/solve.sh
+++ battlefin/bin/solve.sh
@@ -4,10 +4,15 @@ securities=198
 passes=20
 
 rm target/*.*
+echo "computing data..."
 ruby src/ruby/data.rb $securities
+echo "data computed"
 for ((i=1; i<=$securities; i++)) do
    security=o$i
-   vw -d target/$security.train -c --passes $passes -f target/$security.vw
-   vw -d target/$security.test -t -i target/$security.vw -p target/$security.prediction
+   echo "training for security "$security
+   vw -d target/$security.train -c --passes $passes -f target/$security.vw > target/$security.train.out
+   echo "testing for security "$security
+   vw -d target/$security.test -t -i target/$security.vw -p target/$security.prediction > target/$security.test.out
 done
+echo "generating submission file"
 ruby src/ruby/prediction.rb $securities
--- battlefin/src/ruby/data.rb
+++ battlefin/src/ruby/data.rb
@@ -16,26 +16,47 @@ trainLabels = CSV.read("trainLabels.csv")
 for sec in 1..securities
   File.open("target/o"+ sec.to_s + ".train", 'w') do |train|
     for day in traindays
-      line(train, day, trainLabels[day][sec], sec)
+      train.write(trainLabels[day][sec]) # indices are correct!
+      train.write(" | ")
+      for ts in 1..timestamps
+        train.write("o" + sec.to_s + "_" + ts.to_s)
+        train.write(":")
+        train.write(data[day-1][ts][sec-1])
+        train.write(" ")
+      end
+      train.write("day:")
+      train.write(day.to_s)
+      train.write("\n")
     end
   end
   File.open("target/o"+ sec.to_s + ".test", 'w') do |test|
     for day in testdays
-      line(test, day, "0.0", sec)
+      test.write("0.0 | ")
+      for ts in 1..timestamps
+        test.write("o" + sec.to_s + "_" + ts.to_s)
+        test.write(":")
+        test.write(data[day-1][ts][sec-1])
+        test.write(" ")
+      end
+      test.write("day:")
+      test.write(day.to_s)
+      test.write("\n")
     end
   end
 end
 
-def line(file, day, trainValue, sec)
-      file.write(trainValue)
-      file.write(" | ")
+
+def line(file, value, day, sec)
+  train.write(value)
+      train.write(" | ")
       for ts in 1..timestamps
-        file.write("o" + sec.to_s + "_" + ts.to_s)
-        file.write(":")
-        file.write(data[day-1][ts][sec-1])
-        file.write(" ")
+        train.write("o" + sec.to_s + "_" + ts.to_s)
+        train.write(":")
+        train.write(data[day-1][ts][sec-1])
+        train.write(" ")
       end
-      file.write("day:")
-      file.write(day.to_s)
-      file.write("\n")
+      train.write("day:")
+      train.write(day.to_s)
+      train.write("\n")
+    end
 end
